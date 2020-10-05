f2ab43fef4382ce9cc34c56b77b844a5d477d758
Demian George <georgedemian28@gmail.com>
2020-10-05 16:24:06 +0300

    adaugat nr pantof

--- ASC1/Program.cs
+++ ASC1/Program.cs
@@ -11,6 +11,22 @@ namespace ASC1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World of C#!!");
+            Console.WriteLine("introdoceti numarul de la pantof");
+
+            string line;
+            line = Console.ReadLine();
+
+            int numarPantof;
+
+            try
+            {
+                numarPantof = int.Parse(line);
+                Console.WriteLine($"Aveti numarul {numarPantof} la pantof");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
